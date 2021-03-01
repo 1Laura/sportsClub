290e4265b90b87f1b03ce02f944c7fa04481f9f9
Laura Baltrukonė <baltrukone@gmail.com>
2021-03-01 13:06:27 +0200

    env load, db config, set $app

--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
-<h1>Sports club</h1>
+<?php
+require_once '../vendor/autoload.php';
+
+use app\core\Application;
+
+
+// env
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
+$dotenv->load();
+
+$config = [
+    'db' => [
+        'dsn' => $_ENV['DB_DSN'],
+        'user' => $_ENV['DB_USER'],
+        'password' => $_ENV['DB_PASSWORD']
+    ]
+];
+
+
+$app = new Application(dirname(__DIR__));
